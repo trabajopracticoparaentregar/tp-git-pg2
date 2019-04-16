30b14bca55a3f0c909c1371d4fca44bc79cd1966
Juan Guerrero <juanmatiasg@hotmail.com>
2019-04-16 18:58:45 -0300

    Cambiamos el orden de la suma y multiplicacion

--- src/ar/edu/unlampb2/Calculadora.java
+++ src/ar/edu/unlampb2/Calculadora.java
@@ -12,7 +12,7 @@ public class Calculadora {
 		Scanner teclado = new Scanner(System.in);
 
 		do {
-			System.out.println( "Ingrese su operación \n 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir");
+			System.out.println( "Ingrese su operación \n 1 para multiplicar \n 2 para restar \n 3 para sumar \n 4 para dividir");
 			opcion = teclado.nextInt();
 			
 		} while (opcion < 1 && opcion > 4);
@@ -25,7 +25,7 @@ public class Calculadora {
 		
 		switch(opcion){
 			case 1:
-			   System.out.println("El resultado de la suma es :"+(resultado=a+b));
+			   System.out.println("El resultado de la multiplicacion es :"+(resultado=a*b));
 			break;
 			
 			case 2:
@@ -33,7 +33,7 @@ public class Calculadora {
 			break;
 			
 			case 3:
-			   System.out.println("El resultado de la multiplicacion es :"+(resultado=a*b));
+			   System.out.println("El resultado de la suma es :"+(resultado=a+b));
 			break;
 			
 			case 4:
